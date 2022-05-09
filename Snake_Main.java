75575a326840afb3519c057445bd1e16d8ea00dc
Giuseppe Addamo <103690483+CodeDevG@users.noreply.github.com>
2022-05-09 18:27:44 +0200

    Create Snake_Main.java

---
+++ Snake_Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Snake_Main {
+
+	public static void main(String[] args)throws UnsupportedAudioFileException, IOException, LineUnavailableException  {
+		
+		new Game_Window();
+		
+	}
+
+}
